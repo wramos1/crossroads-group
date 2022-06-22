a117bbec363a15e3977d4c558a1f8cdcaca29851
Willyam Ramos <ramoswillyam1@gmail.com>
2022-06-21 20:41:14 -0700

    Incorporated sorting for commits

--- src/components/CommitList.js
+++ src/components/CommitList.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Commit from "./Commit";
 
 const CommitList = ({ commits, owner, repo }) => {
+    const [select, setSelect] = useState('Latest')
 
     const mapCommits = commits.map(c => {
         return (
@@ -14,6 +15,21 @@ const CommitList = ({ commits, owner, repo }) => {
         )
     })
 
+    const reSort = (e) => {
+        setSelect(e.target.value)
+        return commits.reverse().map((c) => {
+            return (
+                <Commit
+                    key={c.node_id}
+                    commit={c.commit.message}
+                    user={c.author.login}
+                    date={c.commit.committer.date}
+                />
+            )
+        })
+    }
+
+
     const renderedList = () => {
         if (!commits) {
             return (
@@ -24,27 +40,33 @@ const CommitList = ({ commits, owner, repo }) => {
         } else if (commits.length < 1) {
             return (
                 <div>
-                    <h1>
+                    <h1 id="noCommitHistoryTitle">
                         Couldn't find that one
                     </h1>
                 </div>
             )
         }
-
         return (
-            < div >
+            <div>
                 <h2>
-                    {`Results for ${owner}'s '${repo}' commits`}
+                    {`History of ${owner}'s '${repo}' commits`}
                 </h2>
+
+                <select id="sortSelect" defaultValue="Latest" onChange={e => reSort(e)}>
+                    <option>Sort By</option>
+                    <option value="Latest">Latest</option>
+                    <option value="Earliest">Earliest</option>
+                </select>
+
                 {mapCommits}
-            </div >
+            </div>
         )
     }
 
     return (
-        <div>
+        < div >
             {renderedList()}
-        </div>
+        </div >
     )
 }
 
