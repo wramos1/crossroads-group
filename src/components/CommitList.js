69386c59055f0f6f89d68864800e6df5d9d00462
Willyam Ramos <ramoswillyam1@gmail.com>
2022-06-21 18:07:10 -0700

    Added conditional rendering and passed owner/repo props

--- src/components/CommitList.js
+++ src/components/CommitList.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Commit from "./Commit";
 
-const CommitList = ({ commits }) => {
+const CommitList = ({ commits, owner, repo }) => {
 
-    const renderedList = commits.map(c => {
-        console.log(c)
+    const mapCommits = commits.map(c => {
         return (
             <Commit
                 key={c.node_id}
@@ -15,9 +14,36 @@ const CommitList = ({ commits }) => {
         )
     })
 
+    const renderedList = () => {
+        if (!commits) {
+            return (
+                <div class="spinner-border text-light" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            )
+        } else if (commits.length < 1) {
+            return (
+                <div>
+                    <h1>
+                        Couldn't find that one
+                    </h1>
+                </div>
+            )
+        }
+
+        return (
+            < div >
+                <h2>
+                    {`Results for ${owner}'s '${repo}' commits`}
+                </h2>
+                {mapCommits}
+            </div >
+        )
+    }
+
     return (
         <div>
-            {renderedList}
+            {renderedList()}
         </div>
     )
 }
